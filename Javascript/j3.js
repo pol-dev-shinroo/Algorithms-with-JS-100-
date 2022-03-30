4177dee921690000bc0659eceaaeb743c565f417
Paul Kim <manus.dream.language@gmail.com>
2022-03-30 16:41:19 +0900

    .

---
+++ Javascript/j3.js
@@ -0,0 +1,5 @@
+const arr = [100, 200, 300];
+console.log(typeof arr);
+
+//  object
+// 정답은 '4번'입니다. undefined, string, number 는 모두 기본 자료형(primitive type) 입니다.
---
+++ Typescript/t3.ts
@@ -0,0 +1,3 @@
+const arr: number[] = [100, 200, 300];
+console.log(typeof arr);
+//  "object"
