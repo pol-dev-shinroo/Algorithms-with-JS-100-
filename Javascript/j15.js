da33e913689e80344c46926f1bd9b0e048e5f671
Paul Kim <manus.dream.language@gmail.com>
2022-03-30 18:56:08 +0900

    .

---
+++ Javascript/j15.js
@@ -0,0 +1,7 @@
+const myName = "김재혁";
+
+let myFunction = (name) => {
+    console.log(`안녕하세요 ${name}님!`);
+};
+
+myFunction(myName);
---
+++ Typescript/t15.ts
@@ -0,0 +1,7 @@
+const myName = "김재혁";
+
+let myFunction = (name: string) => {
+    console.log(`안녕하세요 ${name}님!`);
+};
+
+myFunction(myName);
