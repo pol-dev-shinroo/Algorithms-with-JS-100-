7222a80fb220e3cafc6bdc14dd13a004f9c88cfc
Paul Kim <manus.dream.language@gmail.com>
2022-03-30 23:49:09 +0900

    .

---
+++ Javascript/j37.js
@@ -0,0 +1,17 @@
+const votingResult = (arr) => {
+    let hashMap = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (hashMap.hasOwnProperty(arr[i])) {
+            hashMap[arr[i]] += 1;
+        } else {
+            hashMap[arr[i]] = 1;
+        }
+    }
+    // find dict key with most value
+    let winner = Object.keys(hashMap).reduce(function (a, b) {
+        return hashMap[a] > hashMap[b] ? a : b;
+    });
+    console.log(winner);
+};
+
+votingResult(["원범", "원범", "혜원", "혜원", "혜원", "혜원", "유진", "유진"]);
